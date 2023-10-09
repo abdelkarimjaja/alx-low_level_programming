fa76fadeb9ded230abcfcfea7da5c274c855ecb3
abdelkarimjaja <abdelkarimjaja1@gmail.com>
2023-10-09 02:05:42 -0700

    Hello,printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+/*
+ * @desc: This function aims to print out some text on screen.
+ *
+  */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art\n");
+	return (0);
+}
+
